3387b833e7ef67e0f5ddb13e753eba901bb6009a
Ulia Chelpanova <panda-student@yandex.ru>
2023-12-09 17:06:21 +0300

    Update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { greetUser } from '../src/cli.js';
 import { brainEven } from '../src/gamesRules.js';
-import game from '../src/game.js';
+import game from '../src/index.js';
 
 game(brainEven, greetUser());
