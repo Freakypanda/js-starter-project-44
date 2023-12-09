f1ac736b92b5292c6b6fd77a7d6eaac48cf782ce
Ulia Chelpanova <panda-student@yandex.ru>
2023-12-09 17:40:56 +0300

    Fix import

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { greetUser } from '../src/cli.js';
-import { brainEven } from '../src/gamesRules.js';
+import brainEven from '../src/games/brainEven.js';
 import game from '../src/index.js';
 
 game(brainEven, greetUser());
