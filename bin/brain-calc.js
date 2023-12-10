718906baa7624cf9a81b08d73ca67e0bf467131a
Ulia Chelpanova <panda-student@yandex.ru>
2023-12-10 17:03:17 +0300

    Add new game brain-calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import { brainCalc, rules } from '../src/games/brainCalc.js';
+import game from '../src/index.js';
+
+game(brainCalc, rules);
---
+++ src/games/brainCalc.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import _ from 'lodash';
+
+export const rules = 'What is the result of the expression?';
+export const brainCalc = () => {
+  const operators = ['+', '-', 'x'];
+  const index = _.random(0, 2, false);
+  const num1 = _.random(1, 20, false);
+  const num2 = _.random(1, 20, false);
+  const expression = `${num1} ${operators[index]} ${num2}`;
+  let answer;
+  if (operators[index] === '+') answer = num1 + num2;
+  else if (operators[index] === '-') answer = num1 - num2;
+  else if (operators[index] === 'x') answer = num1 * num2;
+  return [expression, answer.toString()];
+};
